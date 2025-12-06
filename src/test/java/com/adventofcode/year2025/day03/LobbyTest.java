185d5753256e0bd6da362e30e6f0ff06ff41c55f
Nuno Azevedo <nunoaz95@gmail.com>
2025-12-06 19:07:20 +0000

    Add puzzle for 2025 Day 3: Lobby

---
+++ src/test/java/com/adventofcode/year2025/day03/LobbyTest.java
@@ -0,0 +1,15 @@
+package com.adventofcode.year2025.day03;
+
+import com.adventofcode.AbstractPuzzleTest;
+
+public class LobbyTest extends AbstractPuzzleTest<Lobby> {
+    @Override
+    protected long partOneExpectedResult() {
+        return 17100;
+    }
+
+    @Override
+    protected long partTwoExpectedResult() {
+        return 170418192256861L;
+    }
+}
