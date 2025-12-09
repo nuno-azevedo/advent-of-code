59bae103ad2be78b975ebafdaa7bbf0994453215
Nuno Azevedo <nunoaz95@gmail.com>
2025-12-09 20:02:46 +0000

    Add puzzle for 2025 Day 7: Laboratories

---
+++ src/test/java/com/adventofcode/year2025/day07/LaboratoriesTest.java
@@ -0,0 +1,15 @@
+package com.adventofcode.year2025.day07;
+
+import com.adventofcode.AbstractPuzzleTest;
+
+public class LaboratoriesTest extends AbstractPuzzleTest<Laboratories> {
+    @Override
+    protected long partOneExpectedResult() {
+        return 1609;
+    }
+
+    @Override
+    protected long partTwoExpectedResult() {
+        return 12472142047197L;
+    }
+}
